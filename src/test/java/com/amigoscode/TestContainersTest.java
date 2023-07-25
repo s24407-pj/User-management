92c419e226c1d71bb57c1992e7a1aea2e084931d
s24407-pj <s24407@pjwstk.edu.pl>
2023-07-25 20:22:23 +0200

    Renamed class

--- src/test/java/com/amigoscode/TestContainersTest.java
+++ src/test/java/com/amigoscode/TestContainersTest.java
@@ -6,14 +6,10 @@ import org.junit.jupiter.api.Test;
 import static org.assertj.core.api.Assertions.assertThat;
 
 
-public class TestContainersTestContainers extends AbstractTestContainers {
-
-
+public class TestContainersTest extends AbstractTestContainers {
     @Test
     void canStartPostgresDB() {
         assertThat(postgreSQLContainer.isRunning()).isTrue();
         assertThat(postgreSQLContainer.isCreated()).isTrue();
     }
-
-
 }
