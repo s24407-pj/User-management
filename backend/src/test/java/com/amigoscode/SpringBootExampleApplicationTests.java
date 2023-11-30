37ff2d8bb059612f44a38976204023ad7083ea51
Michal <s24407@pjwstk.edu.pl>
2023-11-30 10:26:02 +0100

    Extend SpringBootExampleApplicationTests with AbstractTestContainers

--- backend/src/test/java/com/amigoscode/SpringBootExampleApplicationTests.java
+++ backend/src/test/java/com/amigoscode/SpringBootExampleApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class SpringBootExampleApplicationTests {
+class SpringBootExampleApplicationTests extends AbstractTestContainers {
 
     @Test
     void contextLoads() {
